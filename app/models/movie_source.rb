7f28abf9ef8c1ef679d7a7218193f0cdfd653d79
Daniel Doezema <dan@doezema.com>
2013-12-05 19:48:13 -0500

    Annotations

--- app/models/movie_source.rb
+++ app/models/movie_source.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: movie_sources
+#
+#  id         :integer          not null, primary key
+#  path       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class MovieSource < ActiveRecord::Base
   attr_accessible :path
   has_many :movies
--- test/unit/movie_source_test.rb
+++ test/unit/movie_source_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: movie_sources
+#
+#  id         :integer          not null, primary key
+#  path       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class MovieSourceTest < ActiveSupport::TestCase
